404c08dcb79ae3c5acdf4e76016b7f1b1ea3d83f
Aakash Verma <T_Aakash.Verma@nectechnologies.in>
2020-02-17 14:43:21 +0530

    Role Top Feature Entity Added!

--- src/main/java/com/necindia/rolemanagement/repository/TopFeaturesRepository.java
+++ src/main/java/com/necindia/rolemanagement/repository/TopFeaturesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.necindia.rolemanagement.models.TopFeatures;
 
 public interface TopFeaturesRepository extends JpaRepository<TopFeatures, Integer>{
-
+	
 }
